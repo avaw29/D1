5618e7c49b4629923f9cf1532ff08b0e3f056437
Ava Weiner <avaw@umich.edu>
2018-09-10 09:55:28 -0400

    task 3

--- challenges.py
+++ challenges.py
@@ -5,7 +5,7 @@ import json
 app = Flask(__name__)
 
 
-#Task 2 : Dynamic URLS 
+#Task 2 : Dynamic URLS
     #edit the view function to display 'Welcome to <course_name>' on localhost:5000/course/<course>
 @app.route('/course/<course_name>')
 def courseView(course_name):
@@ -19,7 +19,9 @@ def formView():
     html_form = '''
     <html>
     <body>
-    <form>
+    <form action = "/result" method = "GET">
+        ingredient: <input type = "text" name = "ing"><br>
+        <input type = "submit" value = "Submit">
     </form>
     </body>
     </html>
@@ -29,8 +31,21 @@ def formView():
 #Task 3.2 : Processing Form Data
 @app.route('/result', methods = ['GET', 'POST'])
 def resultView():
-    # Make an API request to Recipe API for the ingredient entered in the form and display the recipe results 
-    return ""
+    # Make an API request to Recipe API for the ingredient entered in the form and display the recipe results
+    if request.method == "GET":
+        ing = request.args.get("ing")
+    base_url = "http://www.recipepuppy.com/api/"
+    params_d = {}
+    params_d ["i"] = ing
+    r = requests.get(base_url, params = params_d)
+    s = json.loads(r.text)
+    recipes = []
+    for rec in s['results']:
+        recipes.append(rec['title'])
+    res = ""
+    for x in recipes:
+        res += '<h1>{}</h1>'.format(x)
+    return res
 
 
 if __name__ == '__main__':
